746fd9cbcff860d16bfed7e6670f3871cf28c793
1Sam Online <csh@korea.com>
2015-02-04 22:26:10 +0900

    Create star_rating_admin.js

---
+++ tpl/js/star_rating_admin.js
@@ -0,0 +1,22 @@
+/**
+ * @file   modules/board/js/board_admin.js
+ * @author NAVER (developers@xpressengine.com)
+ * @brief  star_rating_config 모듈의 관리자용 javascript
+ **/
+
+jQuery(function () {
+
+  jQuery('[data-toggle="tooltip"]').tooltip();
+  //jQuery(".toggle_checkbox").bootstrapSwitch();
+  //jQuery.fn.bootstrapSwitch.defaults.size = 'mini';
+
+/*jQuery('select').on('change', function() {
+	var numberPattern = /\d+/g;
+	var id = (this.name).match( numberPattern );
+
+		jQuery("select[name='star_skin_view[id]]'").value('default');
+  		alert( this.value +'//'+id );
+})*/
+
+
+});
